8c39e17895dd890fd77dcc37339e188f4182915b
Natalie Martinez <64545122+NatiMart@users.noreply.github.com>
2020-06-16 10:15:50 -0400

    update

--- Data Types/Classwork/Cl.js
+++ Data Types/Classwork/Cl.js
@@ -89,157 +89,157 @@
 // function color(colors) {
 //     let count = 0;
     
-//     for(i=0; i < colors.length; i++){
-//         let element = colors[i]; 
+// //     for(i=0; i < colors.length; i++){
+// //         let element = colors[i]; 
 
-//         if (element === 'red') {
-//             count++
-//         } 
-//     }
-//     return count    
-//  }
-//  console.log(color(arr1));
-//  console.log(color(arr2));
-//  console.log(color(arr3));
-// 5/28/20
-    // level 0:
-// let thesky = ['blue', 'red', 'orange', 'grey']
-// let thisyear = ['house', 'certificate','cafe']
+// //         if (element === 'red') {
+// //             count++
+// //         } 
+// //     }
+// //     return count    
+// //  }
+// //  console.log(color(arr1));
+// //  console.log(color(arr2));
+// //  console.log(color(arr3));
+// // 5/28/20
+//     // level 0:
+// // let thesky = ['blue', 'red', 'orange', 'grey']
+// // let thisyear = ['house', 'certificate','cafe']
 
-// function facts(params) {
-//     if (params.length > 3){
-//         return "length greater than 3";
-//     } else {
-//         return "length is 3 or shorter"
-//     }
+// // function facts(params) {
+// //     if (params.length > 3){
+// //         return "length greater than 3";
+// //     } else {
+// //         return "length is 3 or shorter"
+// //     }
      
-// }
-// console.log(facts(thesky));
-// console.log(facts(thisyear));
+// // }
+// // console.log(facts(thesky));
+// // console.log(facts(thisyear));
 
-    // level 1
-// let thesky = ['blue', 'red', 'orange', 'grey']
-// let thisyear = ['house', 'certificate','cafe']
+//     // level 1
+// // let thesky = ['blue', 'red', 'orange', 'grey']
+// // let thisyear = ['house', 'certificate','cafe']
 
 
-// function Larger(params1,params2) {
-//     if (params1.length > params2.length){
-//         return params1;
-//     } else {
-//         return params2;
-//     }
-// }
-// console.log(Larger(thesky,thisyear));
+// // function Larger(params1,params2) {
+// //     if (params1.length > params2.length){
+// //         return params1;
+// //     } else {
+// //         return params2;
+// //     }
+// // }
+// // console.log(Larger(thesky,thisyear));
 
-    //level 2
-// let Jays = ['Just', 'Jump', 'for', 'Joy', 'while', 'Juggling', 'jewels'];
-// let thisyear = ['house', 'certificate','cafe'];
-    // I was getting all uppercase 'J's" and not the lower case one.
-// function letterj(params) {
-//     let collect = []
-//     for (i=0; i < params.length; i++){
-//         if(params[i].indexOf('J') > -1){
-//             collect.push(params[i])
-//         } else if (params[i].indexOf('j') > -1){
-//             collect.push(params[i])
-//         }
+//     //level 2
+// // let Jays = ['Just', 'Jump', 'for', 'Joy', 'while', 'Juggling', 'jewels'];
+// // let thisyear = ['house', 'certificate','cafe'];
+//     // I was getting all uppercase 'J's" and not the lower case one.
+// // function letterj(params) {
+// //     let collect = []
+// //     for (i=0; i < params.length; i++){
+// //         if(params[i].indexOf('J') > -1){
+// //             collect.push(params[i])
+// //         } else if (params[i].indexOf('j') > -1){
+// //             collect.push(params[i])
+// //         }
             
-//     }
-//     return collect
-// }
-    // way 2. got all J's
-// function letterj(params) {
-//     let collect = []
-//     for (i=0; i < params.length; i++){
-//         if(params[i].indexOf('J') > -1 || params[i].indexOf('j') > -1){
-//             collect.push(params[i])
+// //     }
+// //     return collect
+// // }
+//     // way 2. got all J's
+// // function letterj(params) {
+// //     let collect = []
+// //     for (i=0; i < params.length; i++){
+// //         if(params[i].indexOf('J') > -1 || params[i].indexOf('j') > -1){
+// //             collect.push(params[i])
         
-//         }
+// //         }
             
-//     }
-//     return collect
-// }
-    // way 3. simpler way
-// function letterj(params) {
-//     let collect = []
+// //     }
+// //     return collect
+// // }
+//     // way 3. simpler way
+// // function letterj(params) {
+// //     let collect = []
 
-//     for (i=0; i < params.length; i++){
-//         if(params[i].toUpperCase().indexOf('J') > -1 ){
-//             collect.push(params[i])
+// //     for (i=0; i < params.length; i++){
+// //         if(params[i].toUpperCase().indexOf('J') > -1 ){
+// //             collect.push(params[i])
         
-//         }
+// //         }
             
+// //     }
+// //     return collect
+// // }
+// // console.log(letterj(Jays,thisyear));
+// // loops or conditionals (focus on picking ONE or the other)
+
+// // 6.3.20
+
+// // level 0
+// // loop warmup
+//     // create a function which iterates over an array of strings 
+//     // the function returns a new array of captalized strings
+
+//     let vitamins = ["Vitamin B12", "Vitamin C", "Vitamin D"]
+//     function Capitalvites(params) {
+//     let allcaps = []
+//         for (let i = 0; i < params.length; i++) {
+//             let element = params[i];
+//             let Upperel = (element.toUpperCase());            
+//             allcaps.push(Upperel)
+//         }
+
+//         return allcaps
 //     }
-//     return collect
-// }
-// console.log(letterj(Jays,thisyear));
-// loops or conditionals (focus on picking ONE or the other)
-
-// 6.3.20
-
-// level 0
-// loop warmup
-    // create a function which iterates over an array of strings 
-    // the function returns a new array of captalized strings
-
-    let vitamins = ["Vitamin B12", "Vitamin C", "Vitamin D"]
-    function Capitalvites(params) {
-    let allcaps = []
-        for (let i = 0; i < params.length; i++) {
-            let element = params[i];
-            let Upperel = (element.toUpperCase());            
-            allcaps.push(Upperel)
-        }
-
-        return allcaps
-    }
-console.log(Capitalvites(vitamins));
+// console.log(Capitalvites(vitamins));
 
 
-// conditional warmup
-    // create a function which takes in a number 
-    // the function returns 'even' if the number is even and 'odd' if its odd
-    function todo(nums) {
-        if (nums % 2 == 0) {
-            return "even"
-        }
-        else{
-            return "odd"
-        }
-    }
-    console.log(todo(26));
-    console.log(todo(33));
-    console.log(todo(44));
+// // conditional warmup
+//     // create a function which takes in a number 
+//     // the function returns 'even' if the number is even and 'odd' if its odd
+//     function todo(nums) {
+//         if (nums % 2 == 0) {
+//             return "even"
+//         }
+//         else{
+//             return "odd"
+//         }
+//     }
+//     console.log(todo(26));
+//     console.log(todo(33));
+//     console.log(todo(44));
     
 
-// level 1
-    // Q1:
-        // create a function which takes in a string 
-        // the function returns even if the string has an even amount of elements
-        // otherwise it returns odd
-        function BS(params) {
-            if (params.length % 2 == 0) {
-                return "even"
-            }
-            else {
-                return "odd"
-            }
-        }
-        console.log(BS("why"));
-        console.log(BS("go"));
-        console.log(BS("let's rock!"));
-        console.log(BS("maybe"));
+// // level 1
+//     // Q1:
+//         // create a function which takes in a string 
+//         // the function returns even if the string has an even amount of elements
+//         // otherwise it returns odd
+//         function BS(params) {
+//             if (params.length % 2 == 0) {
+//                 return "even"
+//             }
+//             else {
+//                 return "odd"
+//             }
+//         }
+//         console.log(BS("why"));
+//         console.log(BS("go"));
+//         console.log(BS("let's rock!"));
+//         console.log(BS("maybe"));
         
 
-    // Q2: 
-        // create a function which takes in an array
-        // the function returns a new array with every other element in the array
-        let arr = ["Nat","Mad","Jay","Joe"]
-        function really(params) {
-            if (condition) {
+//     // Q2: 
+//         // create a function which takes in an array
+//         // the function returns a new array with every other element in the array
+//         let arr = ["Nat","Mad","Jay","Joe"]
+//         function really(params) {
+//             if (condition) {
                 
-            }
-        }
+//             }
+//         }
 
 
     // Q3:
@@ -284,7 +284,35 @@ console.log(Capitalvites(vitamins));
     // }
 
     // console.log(setDecimalPlaces());
+
+    'abba' , 'baab' 
+
+    'abba' , 'bbaa' 
     
+    'abba' , 'abbba' 
     
+    'abba' , 'abca' 
+
+let anything = "word"
+let idk = anything.split("")
+console.log(idk.sort());
+
+    
+function anagrams(word1, word2) {
+   let word1sort = word1.split("").sort().join("")
+   let word2sort = word2.split("").sort().join("")   
+
+     if (word1sort === word2sort){
+        return "true"
+     } else {
+         return "false"
+     }
+} 
+              
+    
+    console.log(anagrams('abba' , 'baab'));
+    console.log(anagrams('abba' , 'bbaa'));
+    console.log(anagrams('abba' , 'abbba'));
+    console.log(anagrams('abba' , 'abca'));
     
     
